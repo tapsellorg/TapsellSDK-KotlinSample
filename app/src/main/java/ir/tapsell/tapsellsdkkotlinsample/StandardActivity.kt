4b9e34f625deda75fd69b9eaea8ffbf4eee2cd92
msd.khoshkam <msd.khoshkam@gmail.com>
2019-04-09 12:22:38 +0430

    Add standard ad

--- app/src/main/java/ir/tapsell/tapsellsdkkotlinsample/StandardActivity.kt
+++ app/src/main/java/ir/tapsell/tapsellsdkkotlinsample/StandardActivity.kt
@@ -1,12 +1,51 @@
 package ir.tapsell.tapsellsdkkotlinsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import ir.tapsell.sdk.bannerads.TapsellBannerType
+import ir.tapsell.sdk.bannerads.TapsellBannerView
+import kotlinx.android.synthetic.main.activity_standard.*
+import android.view.Gravity
+import android.widget.FrameLayout
+
 
 class StandardActivity : AppCompatActivity() {
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_standard)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        btnStandardBanner.setOnClickListener {
+            bannerView.loadAd(this@StandardActivity, BuildConfig.TAPSELL_STANDARD_BANNER,
+                    TapsellBannerType.BANNER_320x50)
+
+            inflateDynamicalBannerView()
+        }
+    }
+
+    private fun inflateDynamicalBannerView() {
+        val banner = TapsellBannerView(this, TapsellBannerType.BANNER_320x50,
+                BuildConfig.TAPSELL_STANDARD_BANNER)
+
+        val layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams.gravity = Gravity.CENTER_HORIZONTAL
+
+        banner.layoutParams = layoutParams
+        adContainer.addView(banner)
     }
 }
